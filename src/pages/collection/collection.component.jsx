ccee3953c511ef65b5909dfe4d69522cc4585bdb
Renil Babu <renil@infospoke.in>
2020-02-13 12:51:04 +0530

    colledtion route

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 // import CollectionItem from '../../components/collection-item/collection-item.component'
-
+import { selectCollection } from '../../redux/shop/shop.selector'
 
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => (
+const CollectionPage = ({match,collection}) => (
     <div className='collection'>
-        <h2>Collection PAGE</h2>
+        <h2>{collection.id}</h2>
     </div>
 )
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
