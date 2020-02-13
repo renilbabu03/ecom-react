8f66a973cc66ab60e7e5aff3269541726f5b1e70
Renil Babu <renil@infospoke.in>
2020-02-13 13:59:04 +0530

    moved

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import CollectionPreview from '../../components/preview-collection/preview-collection.component'
-import { selectCollections } from '../../redux/shop/shop.selector'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector'
 
 import './collections-overview.styles.scss'
 
@@ -18,6 +18,6 @@ const CollectionsOverview = ({collections}) => (
     </div>
 )
 const mapStateToProps = createStructuredSelector({
-    collections:selectCollections
+    collections:selectCollectionsForPreview
 })
 export default connect(mapStateToProps)(CollectionsOverview);
